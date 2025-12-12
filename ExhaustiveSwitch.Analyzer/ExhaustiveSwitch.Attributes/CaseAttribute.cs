8f9937cd37c531d0510c3045252884daf408b18c
gameshalico <gameshalico@outlook.jp>
2025-12-12 22:22:37 +0900

    Caseがinterfaceにつけられないように

--- ExhaustiveSwitch.Analyzer/ExhaustiveSwitch.Attributes/CaseAttribute.cs
+++ ExhaustiveSwitch.Analyzer/ExhaustiveSwitch.Attributes/CaseAttribute.cs
@@ -6,7 +6,7 @@ namespace ExhaustiveSwitch
     /// 網羅されるべき型に付与する属性。
     /// この属性が付与された型は、[Exhaustive]型に対するswitch処理で明示的に処理される必要があります。
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class CaseAttribute : Attribute
     {
     }
