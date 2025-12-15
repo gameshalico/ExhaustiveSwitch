cb2bc67ba3edf21e3ea9ea019e9e5c9149186c04
gameshalico <gameshalico@outlook.jp>
2025-12-15 15:52:34 +0900

    Update Unity Assets Attribute

--- ExhaustiveSwitch/Assets/ExhaustiveSwitch/Runtime/ExhaustiveAttribute.cs
+++ ExhaustiveSwitch/Assets/ExhaustiveSwitch/Runtime/ExhaustiveAttribute.cs
@@ -5,8 +5,9 @@ namespace ExhaustiveSwitch
     /// <summary>
     /// Attribute to be applied to the type that serves as the base for exhaustiveness checking.
     /// Switch statements on interfaces or abstract classes with this attribute must explicitly handle all types with the [Case] attribute.
+    /// For enums, all enum members must be explicitly handled in switch statements.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
     public sealed class ExhaustiveAttribute : Attribute
     {
     }
