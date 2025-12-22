564264801ef4f19428171e4c1b7876aacc248aac
gameshalico <gameshalico@outlook.jp>
2025-12-22 19:55:40 +0900

    change AttributeUsage for struct

--- ExhaustiveSwitch/Assets/ExhaustiveSwitch/Runtime/CaseAttribute.cs
+++ ExhaustiveSwitch/Assets/ExhaustiveSwitch/Runtime/CaseAttribute.cs
@@ -6,7 +6,7 @@ namespace ExhaustiveSwitch
     /// Attribute to be applied to types that should be exhaustively handled.
     /// Types with this attribute must be explicitly handled in switch statements on [Exhaustive] types.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     public sealed class CaseAttribute : Attribute
     {
     }
